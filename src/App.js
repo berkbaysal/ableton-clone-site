90e4dfbb18289b00bd02c20ea66cfbcef45aca6a
berkbaysal <george@do.com>
2022-07-29 20:17:12 +0300

    video,2-image,info-box sections added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react"
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero"
+import Infobox from "./components/Infobox";
+import TwoImages from "./components/TwoImages"
+import ImageOne from "./assets/img/two-images-one.jpg"
+import ImageTwo from "./assets/img/two-images-two.jpg"
+import Videobox from "./components/Videobox";
 
 function App() {
 
@@ -8,6 +13,10 @@ function App() {
         <main>
             <Navbar />
             <Hero />
+            <Infobox />
+            <TwoImages backgroundAlign="right" content={[ImageOne,ImageTwo]} backgroundColor = "#fbffa7"/>
+            <Infobox />
+            <Videobox width="66%" videoId="BC19kwABFwc" label="A delightful music video"/>
         </main>
     );
 }
---
+++ src/components/Infobox.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Infobox(props) {
+    return (
+        <div className="ableton-style-infobox">
+            <p className="top-line">
+                We make <a href="" className="inline-link">Live</a>, Push and Link — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.
+            </p>
+            <p className="bottom-line">
+            Ableton was founded in 1999 and released the first version of Live in 2001. Our products are used by a community of dedicated musicians, sound designers, and artists from across the world.
+            </p>
+        </div>
+    );
+}
+
+export default Infobox;
